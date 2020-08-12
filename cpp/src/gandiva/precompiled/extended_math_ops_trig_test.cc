5baa9955b2d26f48b810115e58f2967a80eb5023
Anton Alkin <anton.alkin@cern.ch>
2020-08-12 10:35:01 +0200

    Do not use M_PI_# constants that are not portable

--- cpp/src/gandiva/precompiled/extended_math_ops_trig_test.cc
+++ cpp/src/gandiva/precompiled/extended_math_ops_trig_test.cc
@@ -25,44 +25,44 @@
 namespace gandiva {
 
 TEST(TestExtendedMathOps, TestSine) {
-  VerifyFuzzyEquals(sin_float32(M_PI_2), 1);
-  VerifyFuzzyEquals(sin_float64(M_PI_2), 1);
-  VerifyFuzzyEquals(sin_float32(M_PI_4), 0.7071);
-  VerifyFuzzyEquals(sin_float64(M_PI_4), 0.7071);
+  VerifyFuzzyEquals(sin_float32(M_PI / 2.), 1.);
+  VerifyFuzzyEquals(sin_float64(M_PI / 2.), 1.);
+  VerifyFuzzyEquals(sin_float32(M_PI / 4.), 0.7071);
+  VerifyFuzzyEquals(sin_float64(M_PI / 4.), 0.7071);
 }
 
 TEST(TestExtendedMathOps, TestCosine) {
-  VerifyFuzzyEquals(cos_float32(M_PI_2), 0);
-  VerifyFuzzyEquals(cos_float64(M_PI_2), 0);
-  VerifyFuzzyEquals(cos_float32(M_PI_4), 0.7071);
-  VerifyFuzzyEquals(cos_float64(M_PI_4), 0.7071);
+  VerifyFuzzyEquals(cos_float32(M_PI / 2.), 0.);
+  VerifyFuzzyEquals(cos_float64(M_PI / 2.), 0.);
+  VerifyFuzzyEquals(cos_float32(M_PI / 4.), 0.7071);
+  VerifyFuzzyEquals(cos_float64(M_PI / 4.), 0.7071);
 }
 
 TEST(TestExtendedMathOps, TestTangent) {
   VerifyFuzzyEquals(tan_float32(M_PI / 3.), 1.73205);
   VerifyFuzzyEquals(tan_float64(M_PI / 3.), 1.73205);
-  VerifyFuzzyEquals(tan_float32(M_PI_4), 1);
-  VerifyFuzzyEquals(tan_float64(M_PI_4), 1);
+  VerifyFuzzyEquals(tan_float32(M_PI / 4.), 1.);
+  VerifyFuzzyEquals(tan_float64(M_PI / 4.), 1.);
 }
 
 TEST(TestExtendedMathOps, TestASine) {
-  VerifyFuzzyEquals(asin_float32(1), M_PI_2);
-  VerifyFuzzyEquals(asin_float64(1), M_PI_2);
-  VerifyFuzzyEquals(asin_float32(0.7071), M_PI_4);
-  VerifyFuzzyEquals(asin_float64(0.7071), M_PI_4);
+  VerifyFuzzyEquals(asin_float32(1.), M_PI / 2.);
+  VerifyFuzzyEquals(asin_float64(1.), M_PI / 2.);
+  VerifyFuzzyEquals(asin_float32(0.7071), M_PI / 4.);
+  VerifyFuzzyEquals(asin_float64(0.7071), M_PI / 4.);
 }
 
 TEST(TestExtendedMathOps, TestACosine) {
-  VerifyFuzzyEquals(acos_float32(0), M_PI_2);
-  VerifyFuzzyEquals(acos_float64(0), M_PI_2);
-  VerifyFuzzyEquals(acos_float32(0.7071), M_PI_4);
-  VerifyFuzzyEquals(acos_float64(0.7071), M_PI_4);
+  VerifyFuzzyEquals(acos_float32(0.), M_PI / 2.);
+  VerifyFuzzyEquals(acos_float64(0.), M_PI / 2.);
+  VerifyFuzzyEquals(acos_float32(0.7071), M_PI / 4.);
+  VerifyFuzzyEquals(acos_float64(0.7071), M_PI / 4.);
 }
 
 TEST(TestExtendedMathOps, TestATangent) {
   VerifyFuzzyEquals(atan_float32(1.73205), M_PI / 3.);
   VerifyFuzzyEquals(atan_float64(1.73205), M_PI / 3.);
-  VerifyFuzzyEquals(atan_float32(1), M_PI_4);
-  VerifyFuzzyEquals(atan_float64(1), M_PI_4);
+  VerifyFuzzyEquals(atan_float32(1.), M_PI / 4.);
+  VerifyFuzzyEquals(atan_float64(1.), M_PI / 4.);
 }
 }  // namespace gandiva
