a8e12175cf54d0c26feb4c7f745b13448294233b
Anton Alkin <anton.alkin@cern.ch>
2020-08-12 11:18:23 +0200

    Explicitly define M_PI

--- cpp/src/gandiva/precompiled/extended_math_ops_trig_test.cc
+++ cpp/src/gandiva/precompiled/extended_math_ops_trig_test.cc
@@ -22,6 +22,10 @@
 #include "gandiva/execution_context.h"
 #include "gandiva/precompiled/types.h"
 
+#ifndef M_PI
+    #define M_PI 3.14159265358979323846
+#endif
+
 namespace gandiva {
 
 TEST(TestExtendedMathOps, TestSine) {
